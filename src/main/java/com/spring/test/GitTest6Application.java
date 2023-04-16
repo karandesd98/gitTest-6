d351dc1181921d676e054ca5024e1f168330bc82
sachin karande <karandesd98@gmail.com>
2023-04-16 03:29:03 -0700

    uploading project to git hub

---
+++ src/main/java/com/spring/test/GitTest6Application.java
@@ -0,0 +1,13 @@
+package com.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTest6Application.class, args);
+	}
+
+}
